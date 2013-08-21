c9a6ce9e4f045dd3f8319b0558f60e11d849a409
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2013-08-21 15:18:56 +0200

    [install+ssh] SSH config

--- install.sh
+++ install.sh
@@ -60,7 +60,8 @@ ln -sf $DOTFILES/git/gitconfig ~/.gitconfig
 
 mkdir -p ~/.ssh/sockets
 ln -sf $DOTFILES/ssh/sshpubkey ~/.ssh/sshpubkey
-[[ ! -f ~/.ssh/config ]] && ln -sf $DOTFILES/ssh/config ~/.ssh/config
+[[ ! -f ~/.ssh/config ]] && cat $DOTFILES/ssh/config{,.*} > ~/.ssh/config
+
 ## aurvote
 
 mkdir -p ~/.config
