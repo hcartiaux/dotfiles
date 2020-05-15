44e068bc092ff515dbca2ae6a512e9b6f61a4da5
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2020-05-15 18:18:53 +0200

    [direnv] Remove the useless space in the PS1 var

--- direnv/init.sh
+++ direnv/init.sh
@@ -8,7 +8,7 @@ if [ -n "$(which direnv 2>/dev/null)" ]; then
     # See https://github.com/direnv/direnv/wiki/Python#restoring-the-ps
     show_virtual_env() {
         if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
-            echo "($(basename $VIRTUAL_ENV))"
+            echo "($(basename $VIRTUAL_ENV)) "
         fi
     }
     if [[ -n ${ZSH_VERSION-} ]]; then
@@ -17,5 +17,5 @@ if [ -n "$(which direnv 2>/dev/null)" ]; then
         export -f show_virtual_env
     fi
 
-    PS1='$(show_virtual_env) '$PS1
+    PS1='$(show_virtual_env)'$PS1
 fi
