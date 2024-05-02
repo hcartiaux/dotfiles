e371041565e7beae9d68c1fcf403a3751969b8c2
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2024-05-03 00:40:19 +0200

    [install] do not create the g5k ssh config

--- install.sh
+++ install.sh
@@ -82,7 +82,6 @@ ln -sf $DOTFILES/git/gitconfig-personal ~/.gitconfig-personal
 mkdir -p  ~/.ssh/sockets
 ln  -sf $DOTFILES/ssh/sshpubkey       ~/.ssh/sshpubkey
 cat     $DOTFILES/ssh/config.*      > ~/.ssh/config.perso
-cat     $DOTFILES/ssh/config{,.g5k} > ~/.ssh/config_g5k
 [[ ! -f ~/.ssh/config ]] && (
     cat $DOTFILES/ssh/config{,.*}   > ~/.ssh/config
 )
