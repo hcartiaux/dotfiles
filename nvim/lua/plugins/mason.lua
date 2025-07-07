3a66046bea4af39991a887f726170ef8cec5d11c
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-07-08 00:26:42 +0200

    [nvim] mason - remove utils installed redundantly

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -5,15 +5,10 @@ return {
     opts = {
       ensure_installed = {
         "bash-language-server",
-        "shellcheck",
-        "shfmt",
-        "flake8",
         "systemd-language-server",
         "ansible-language-server",
-        "ansible-lint",
         "yaml-language-server",
-        "ast-grep",
-        "python-lsp-server"
+        "python-lsp-server",
       },
     },
   },
