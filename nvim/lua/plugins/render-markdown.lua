2abb74b8082d8b07aa9a031f2bcad0ee2de57d14
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2025-07-08 00:27:28 +0200

    [nvim] Enable render-markdown

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,6 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use the mini.nvim suite
+  ft = { "markdown", "codecompanion" },
+  opts = {},
+}
