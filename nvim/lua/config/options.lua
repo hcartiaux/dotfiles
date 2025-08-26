93aeba0f0ccfd168c88e6e4bbfe6375036c5a46e
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-08-26 14:29:22 +0200

    [nvim] options - enable permanent undo and use custom directory for undo backup and swap files

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,28 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Do not autoformat on save
 vim.g.autoformat = false
+
+-- Enable Neovim undo, backup, and swap files in custom directories
+local data_path = vim.fn.stdpath("data")
+local swap_dir = data_path .. "/swap/"
+local backup_dir = data_path .. "/backup/"
+local undo_dir = data_path .. "/undo/"
+
+local function ensure_dirs(...)
+  for _, dir in ipairs({ ... }) do
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p", "0o700")
+    end
+  end
+end
+ensure_dirs(swap_dir, backup_dir, undo_dir)
+
+vim.opt.directory = swap_dir
+vim.opt.backupdir = backup_dir
+vim.opt.undodir = undo_dir
+vim.opt.swapfile = true
+vim.opt.backup = true
+vim.opt.undofile = true
