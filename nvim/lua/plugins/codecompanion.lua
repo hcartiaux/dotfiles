31cb2f1e792fe9da8aef73910cfa6435c139ebf6
Hyacinthe Cartiaux <hyacinthe.cartiaux@free.fr>
2026-02-18 22:41:08 +0100

    [nvim] codecompanion - use mistral by default

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -2,20 +2,6 @@ return {
   "olimorris/codecompanion.nvim",
   opts = {
     adapters = {
-      gemini = function()
-        return require("codecompanion.adapters").extend("gemini", {
-          env = {
-            api_key = "cmd:echo -n $GEMINI_API_KEY",
-          },
-        })
-      end,
-      anthropic = function()
-        return require("codecompanion.adapters").extend("anthropic", {
-          env = {
-            api_key = "cmd:echo -n $ANTHROPIC_API_KEY",
-          },
-        })
-      end,
       mistral = function()
         return require("codecompanion.adapters").extend("mistral", {
           env = {
@@ -23,26 +9,16 @@ return {
           },
         })
       end,
-      qwen3 = function()
-        return require("codecompanion.adapters").extend("ollama", {
-          name = "qwen3-coder",
-          schema = {
-            model = {
-              default = "qwen3-coder:latest",
-            },
-          },
-        })
-      end,
     },
     strategies = {
       chat = {
-        adapter = "qwen3",
+        adapter = "mistral",
       },
       inline = {
-        adapter = "qwen3",
+        adapter = "mistral",
       },
       cmd = {
-        adapter = "qwen3",
+        adapter = "mistral",
       }
     },
   },
